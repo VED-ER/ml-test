55a2d576ff70848d4cf45e0224d1276da48b6beb
Vedran Erak <vedran1.erak1@gmail.com>
2024-07-12 14:19:41 +0200

    Initial setup

---
+++ app/index.tsx
@@ -0,0 +1,43 @@
+import { Button, Image, StyleSheet, Text, View } from "react-native"
+import * as ImagePicker from "expo-image-picker"
+import { useState } from "react"
+
+export default function Index() {
+    const [image, setImage] = useState("")
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri)
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Button
+                title="Pick an image from camera roll"
+                onPress={pickImage}
+            />
+            {image && <Image source={{ uri: image }} style={styles.image} />}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
+})
