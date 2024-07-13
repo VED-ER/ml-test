acbbbab036a490f206a73f19ae7f447a4b05fb0a
Vedran Erak <vedran1.erak1@gmail.com>
2024-07-13 17:06:25 +0200

    Add support for taking a picture with camera

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
-import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native"
+import {
+    Alert,
+    Button,
+    Image,
+    ScrollView,
+    StyleSheet,
+    Text,
+} from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import { useState } from "react"
 import { recognizeTextFromImage } from "@/modules/text-recognition"
 
-export default function Index() { 
+export default function Index() {
     const [image, setImage] = useState("")
     const [recognizedText, setRecognizedText] = useState("")
 
+    const [status, requestPermission] = ImagePicker.useCameraPermissions()
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
@@ -18,20 +26,48 @@ export default function Index() {
 
         if (!result.canceled) {
             setImage(result.assets[0].uri)
+            const recText = await recognizeText(result.assets[0].uri)
+            setRecognizedText(recText || "")
+        }
+    }
 
-            try {
-                const imgText = await recognizeTextFromImage(
-                    result.assets[0].uri
-                )
-                setRecognizedText(imgText)
-            } catch (error) {
-                console.log("err")
+    async function recognizeText(uri: string) {
+        try {
+            const imgText = await recognizeTextFromImage(uri)
+            return imgText
+        } catch (error) {
+            console.log("err")
+        }
+    }
+
+    async function onOpenCameraPress() {
+        let permissionResult
+        if (!status) {
+            permissionResult = await requestPermission()
+            if (!permissionResult.granted) {
+                Alert.alert("Access needed", "Allow access in settings")
+                return
+            }
+        }
+
+        if (status?.granted) {
+            const result = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            })
+
+            if (!result.canceled) {
+                setImage(result.assets[0].uri)
+                const recText = await recognizeText(result.assets[0].uri)
+                setRecognizedText(recText || "")
             }
         }
     }
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
+            <Button title="Open Camera" onPress={onOpenCameraPress} />
             <Button
                 title="Pick an image from camera roll"
                 onPress={pickImage}
